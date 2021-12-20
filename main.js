d9a9174c3cc95e4f187b3eb608e1013633788a2d
MUHAMMAD <shakibrahman6767@gmail.com>
2021-12-20 14:39:26 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// wow animation
+
+new WOW().init();
+
+// humburger menu
+
+function openMenu (){
+    document.getElementById('hb').style.width="100%";
+}
+function closeMenu (){
+    document.getElementById('hb').style.width="0%";
+}
+
+
+// mixitup
+
+var mixer = mixitup('.work-filter-grid');
